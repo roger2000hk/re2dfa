511c98dad35013e07a86d18c94a10481b84c1be1
opennota <opennota@gmail.com>
2015-06-24 00:19:20 +0700

    Add package comment

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -11,6 +11,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Package codegen implements generation of Go code from deterministic finite automata.
 package codegen
 
 import (
